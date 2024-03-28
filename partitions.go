97a1f7b20b4b4362e84749b20cc4c4cf603c709a
tung.tq <tung.tq@teko.vn>
2024-03-28 17:00:10 +0700

    Tmp

--- partitions.go
+++ partitions.go
@@ -1,1 +1,25 @@
 package shardt
+
+func allocatePartitions(count int, addrs []string, previous []string) []string {
+	min := count / len(addrs)
+	max := min + 1
+
+	maxCount := count - min*len(addrs)
+
+	previousAlloc := map[string][]int
+
+	var result []string
+	for i, addr := range addrs {
+		if i < maxCount {
+			for k := 0; k < max; k++ {
+				result = append(result, addr)
+			}
+		} else {
+			for k := 0; k < min; k++ {
+				result = append(result, addr)
+			}
+		}
+	}
+
+	return result
+}
--- partitions_test.go
+++ partitions_test.go
@@ -1,1 +1,45 @@
 package shardt
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAllocatePartitions(t *testing.T) {
+	table := []struct {
+		name     string
+		count    int
+		addrs    []string
+		previous []string
+		expected []string
+	}{
+		{
+			name:     "single-addr",
+			count:    3,
+			addrs:    []string{"addr1"},
+			previous: []string{"", "", ""},
+			expected: []string{"addr1", "addr1", "addr1"},
+		},
+		{
+			name:     "two-addrs",
+			count:    3,
+			addrs:    []string{"addr1", "addr2"},
+			previous: []string{"", "", ""},
+			expected: []string{"addr1", "addr1", "addr2"},
+		},
+		{
+			name:     "previous-only-addr2",
+			count:    3,
+			addrs:    []string{"addr1", "addr2"},
+			previous: []string{"addr2", "addr2", "addr2"},
+			expected: []string{"addr2", "addr1", "addr1"},
+		},
+	}
+
+	for _, e := range table {
+		t.Run(e.name, func(t *testing.T) {
+			result := allocatePartitions(e.count, e.addrs, e.previous)
+			assert.Equal(t, e.expected, result)
+		})
+	}
+}
